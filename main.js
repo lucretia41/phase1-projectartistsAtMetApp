18e23c6a65d9b1ae186715e3641c07ae41717f46
Ian Scott Martin <ianscottmartin@gmail.com>
2023-04-29 10:19:44 -0600

    able to access MET with fetch

--- main.js
+++ main.js
@@ -2,10 +2,48 @@ console.log("works")
 fetch("http:localhost:3000/artworks")
     .then(response => response.json())
     .then(artworks => {
-      const container = document.querySelector("ul#artworkList");  
-      artworks.forEach(artwork => {
-      })
-      
-      });
+        const container = document.querySelector("ul#artworkList");
+        //   artworks.forEach(artwork => {
+        //   })
+
+        //   });
+    })
+
+fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (title) {
+        console.log(title);
+
+    });
+// fetch("https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]")
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (objectID) {
+//         console.log(objectID);
+//     });
+// fetch("https://collectionapi.metmuseum.org/public/collection/v1/search")
+//     .then(function (res) {
+//         return res.json();
+
+//     })
+//     .then(function (objectsIDs) {
+//         console.log(objectsIDs);
+//     });
+
+fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?isOnView=true&q=vincent van gogh")
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (isOnView) {
+        console.log(isOnView)
+    });
+
+
+
+
+
 
 
